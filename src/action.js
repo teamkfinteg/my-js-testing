58a936a72a9135d52cb680b34c8969df727bd1fc
Mikey Henderson <fiddlermikey@gmail.com>
2023-01-18 13:48:20 -0800

    Update action.js

--- src/action.js
+++ src/action.js
@@ -4,7 +4,7 @@ const github = require('@actions/github');
 async function run() {
 	console.log('Hello, world!');
 	const GITHUB_TOKEN = core.getInput('GITHUB_TOKEN');
-	const OCTOKIT = github.getOctokit(GITHUB_TOKEN);
+	const octokit = github.getOctokit(GITHUB_TOKEN);
 
 	const { context = {} } = github;
 	const { pull_request } = context.payload;
